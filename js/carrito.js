e27115e0d9f54d143320786a956c595ed0c6cf62
TomiAllende <tomasallende2001@gmail.com>
2022-08-31 09:24:25 -0300

    realizo entrega

--- js/carrito.js
+++ js/carrito.js
@@ -12,31 +12,118 @@ console.log(resultado)
 return resultado
 }
 
-//Armo array sacando y agregando pedidos
+// En funcion a los pedidos que haya en e carro los sumo para obtener el total.
 
-const pedidx = ["Pizza de cebolla", "Empanada JQ", "cerveza schenider"]
-pedidx.push ("empanada de carne")
-pedidx.push("cerveza stella")
-console.log(pedidxs.length)
+const { $dataMetaSchema } = require("ajv")
 
-pedidx.shift
-console.log(pedidx)
+//Los num son los prodcutos
 
-//Intento sumar pedidos a mi carrito
- 
-let less =document.querySelector(".menos")
-let more = document.querySelector(".mas")
-let resultado =document.querySelector(".resultado")
-let cuenta = 0
-
-more.onclick = (e) => {
-    console.log(e)
-    cuenta += 1
-    resultado.innerText = cuenta
+function sumar(num1, num2, num3) {
+    return num1 + num2 + num3
 }
 
-less.addEventListener("click", function(e){
-    console.log(e)
-    cuenta -= 1
-    resultado.innerText = cuenta
+function calculos(operacion, num1, num2, num3) {
+    let resultado = operacion(num1, num2, num3)
+    console.log(resultado)
+    return resultado
+}
+
+
+const productos = [
+    {
+        id: 1,
+        nombre: "Pizzas",
+        precio: 600,
+        sabores: "cebolla,jamon,muzza",
+
+    },
+
+    {
+        id: 2,
+        nombre: "Empanadas",
+        precio: 250,
+        sabores: "cebolla, carne, jamon y queso",
+    },
+    {
+        id: 3,
+        nombre: "Cervezas",
+        precio: 1200,
+        sabores: "stella, heineken, schneider, imperial, quilmes",
+    },
+]
+
+let storage = []
+
+function carritoproducts(id, insertBox) {
+    let producto = productos[id - 1]
+    const { nombre, precio, sabores } = producto
+
+    insertBox.innerHTML = <div>
+        <div class="cerrarcarro">Cerrar</div>
+            <div class="click">
+                <span class="menos">-</span>
+                <span class="resultado">0</span>
+                <span class="mas">+</span>
+            </div>
+            <div class="botonCarrito">AGREGAR</div>
+    
+        </div>
+
+    
+
+
+    //Intento sumar pedidos a mi carrito
+const cerrarcarro = document.querySelector(".cerrarcarro")
+    const less = document.querySelector(".menos")
+    const more = document.querySelector(".mas")
+    const resultado = document.querySelector(".resultado")
+    const agregarCarrito = document.querySelector(".botonCarrito")
+    let cuenta = 0
+
+    more.onclick = () => {
+        cuenta ++
+        cuenta += 1
+        resultado.innerText = cuenta
+    }
+
+    less.onclick= () => {
+        cuenta --
+        cuenta -= 1
+        resultado.innerText = cuenta
+    }
+    agregarCarrito.onclick = () => {
+        if(cuenta !=0){
+            producto.sumacarro = cuenta
+            storage.push(producto)
+            sessionStorage.setItem("carrito", JSON.stringify(storage))
+sweetAlert({
+    title: "Agregaste $(producto.agregarCarrito) ${nombre} a tu carrito!",
+    icon: "success",
+confirmButtonText: "Enharoabuena",
 })
+        }
+        else{
+            sweetAlert({
+                title: "Error",
+                text: "No podes dejar vacio",
+                icon: "error",
+                confirmButtonText: "Mal",
+            })
+        }
+    } 
+}
+
+const cajaprincipal = document.querySelector(".main")
+for (producto of productos){
+    const {id, nombre, precio, sabores} = producto
+
+    cajaprincipal.innerHTML += <article class="product">
+        <div class="descripcion">
+            <h3></h3>
+            <div class="boton">AGREGAR</div>
+        </div>
+        </article>
+  
+}
+
+const BotonMirar = document.querySelector(".boton")
